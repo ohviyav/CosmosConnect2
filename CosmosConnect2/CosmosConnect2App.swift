7c22c52fceaaf8644d04d24d80cab21999c8bf32
Ohviya Velu <ohviya.pugal@gmail.com>
2025-07-16 15:29:34 -0500

    Initial Commit

---
+++ CosmosConnect2/CosmosConnect2App.swift
@@ -0,0 +1,17 @@
+//
+//  CosmosConnect2App.swift
+//  CosmosConnect2
+//
+//  Created by Scholar on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CosmosConnect2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
